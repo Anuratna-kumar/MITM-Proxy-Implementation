af6e69b5ddb64e1506f038c5a019a1543de77093
Anuratna <anuratna786@gmail.com>
2021-03-01 17:32:00 +0530

    reading mocked dat througha file

--- main.py
+++ main.py
@@ -11,17 +11,20 @@ def convert_to_json():
         req = json.loads(file.read())
 
 
+convert_to_json()
+
+
 def response(flow: http.HTTPFlow):
-    convert_to_json()
-    if req["breakpoint_url"] in flow.request.pretty_url:
-        new_response = httpModifier.RequestModifier(flow)
-        new_response.set_response_status_code(req["mocked_response_code"])
-        new_response.set_response_body(str(req["mocked_response_body"]))
-        log.log_network(flow.response)
+    for r in req:
+        if r["breakpoint_url"] in flow.request.pretty_url:
+            new_response = httpModifier.RequestModifier(flow)
+            new_response.set_response_status_code(r["mocked_response_code"])
+            new_response.set_response_body(str(r["mocked_response_body"]))
+            log.log_network(flow.response)
 
 
 def request(flow: http.HTTPFlow):
-    convert_to_json()
-    if req["breakpoint_url"] in flow.request.pretty_url:
-        new_request = httpModifier.RequestModifier(flow)
-        new_request.set_request_header(req["mocked_request_header"])
+    for r in req:
+        if r["breakpoint_url"] in flow.request.pretty_url:
+            new_request = httpModifier.RequestModifier(flow)
+            new_request.set_request_header(r["mocked_request_header"])
